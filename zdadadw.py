dafcbb975855b10247201ca024a96dbb799cf1a5
Mylesmile <138273710+Mylesmile@users.noreply.github.com>
2023-07-15 14:47:12 +0900

    Add files via upload

---
+++ zdadadw.py
@@ -0,0 +1,20 @@
+class Car :
+    def __init__(self, model, color) :
+        self.model = model
+        self.color = color
+
+    def info(self) :
+        print("Model : ", self.model, ", Color :", self.color)
+
+class CarDrive(Car) :
+
+    def info(self) :
+        print("The model of this car is %s." %self.model)
+        print("The color is %s." % self.color)
+
+    def speedChange(self, v) :
+        self.speed = v
+        print("speedChange :", self.speed)
+
+bmw = CarDrive("BMW", "white")
+bmw.info()
